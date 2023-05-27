91997c7d2749d26f723f5335f669962bed6fbcde
Alen <132235388+fragN7@users.noreply.github.com>
2023-05-27 21:29:56 +0300

    Added AI Model and endpoint

---
+++ VSLab/Controllers/AIModelController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using VSLab.ChessAiModel;
+
+namespace VSLab.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AIModelController : ControllerBase
+    {
+        [HttpGet("{rating}/{tournaments}/{champions}")]
+        [AllowAnonymous]
+        public IActionResult GetChampionPrediction(int rating, int tournaments, int champions)
+        {
+            // Load sample data
+            //Load sample data
+            var sampleData = new MLModel.ModelInput()
+            {
+                Id = 3F,
+                Rating = rating,
+                Tournaments = tournaments,
+                Champions = champions,
+            };
+
+            //Load model and predict output
+            var result = MLModel.Predict(sampleData);
+
+            return Ok(result);
+        }
+    }
+}
